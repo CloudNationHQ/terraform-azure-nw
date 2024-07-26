82efb3fde8eb18d70bebc85519671e5d6c118bdc
Tobias <tobias.vanstokkum@cloudnation.nl>
2024-07-26 16:54:03 +0200

    feat: add outputs

--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,9 @@
+output "watcher" {
+  description = "contains all network watcher details"
+  value       = azurerm_network_watcher.nwatcher
+}
 
+output "flowlog" {
+  description = "contains all network watcher flow log details"
+  value       = azurerm_network_watcher_flow_log.nwatcher_flowlog
+}
