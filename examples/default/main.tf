64a6953c4683308542458fbcce4e769e23aa77a5
Dennis Kool <43271994+dkooll@users.noreply.github.com>
2024-09-24 11:51:18 +0200

    feat!: upgrade azurerm provder to v4 (#8)
    BREAKING CHANGE: * Version 4 of the azurerm provider includes breaking changes.
    
    Co-authored-by: dkool <dkool@dkools-MacBook-Pro.local>

--- examples/default/main.tf
+++ examples/default/main.tf
@@ -7,38 +7,38 @@ module "naming" {
 
 module "rg" {
   source  = "cloudnationhq/rg/azure"
-  version = "~> 0.1"
+  version = "~> 2.0"
 
   groups = {
     demo = {
-      name   = module.naming.resource_group.name
-      region = "westeurope"
+      name     = module.naming.resource_group.name
+      location = "westeurope"
     }
   }
 }
 
 module "storage" {
   source  = "cloudnationhq/sa/azure"
-  version = "~> 0.1"
+  version = "~> 2.0"
 
   storage = {
-    name          = module.naming.storage_account.name_unique
-    location      = module.rg.groups.demo.location
-    resourcegroup = module.rg.groups.demo.name
+    name           = module.naming.storage_account.name_unique
+    location       = module.rg.groups.demo.location
+    resource_group = module.rg.groups.demo.name
   }
 }
 
 module "network" {
   source  = "cloudnationhq/vnet/azure"
-  version = "~> 2.7"
+  version = "~> 4.0"
 
   naming = local.naming
 
   vnet = {
-    name          = module.naming.virtual_network.name
-    cidr          = ["10.18.0.0/16"]
-    location      = module.rg.groups.demo.location
-    resourcegroup = module.rg.groups.demo.name
+    name           = module.naming.virtual_network.name
+    cidr           = ["10.18.0.0/16"]
+    location       = module.rg.groups.demo.location
+    resource_group = module.rg.groups.demo.name
 
     subnets = {
       sn1 = {
@@ -51,7 +51,7 @@ module "network" {
 
 module "watcher" {
   source  = "cloudnationhq/nw/azure"
-  version = "~> 0.1"
+  version = "~> 1.0"
 
   watchers = {
     watcher = {
